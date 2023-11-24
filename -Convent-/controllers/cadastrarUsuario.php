427449f9ffc7bb430bc39573bf728e71580eac1c
Igor Bruno <131173396+Rohnin08@users.noreply.github.com>
2023-11-24 07:51:21 -0300

    Update cadastrarUsuario.php

--- -Convent-/controllers/cadastrarUsuario.php
+++ -Convent-/controllers/cadastrarUsuario.php
@@ -1,24 +1,3 @@
-// <?php 
-
-// require_once('../config/connect.php');
-// require_once('../index.php');
-
-// if(hasUser()) {
-//     header('Location: /pages/dashbord.php');
-// }
-
-// if(hasUser()) {
-//     header('location: /cadastro');
-// }
-
-// if (isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
-//     $nome = $_POST('nome');
-//     $email = $_POST('email');
-//     $senha = $_POST('senha');
-//     $db = connectDatabase();
-//     $db -> exec("INSERT INTO (user_name, user_email, user_senha) values('".$nome."', '".$email."', '".$senha."')");
-// }
-
 <?php
 
 require_once('../config/connect.php');
