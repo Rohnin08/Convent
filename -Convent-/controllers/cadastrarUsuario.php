2e921ab3ca367512c362d4218186d8b2471742d8
Igor Bruno <131173396+Rohnin08@users.noreply.github.com>
2023-11-24 07:38:09 -0300

    Update cadastrarUsuario.php

--- -Convent-/controllers/cadastrarUsuario.php
+++ -Convent-/controllers/cadastrarUsuario.php
@@ -1,20 +1,38 @@
-<?php 
+// <?php 
+
+// require_once('../config/connect.php');
+// require_once('../index.php');
+
+// if(hasUser()) {
+//     header('Location: /pages/dashbord.php');
+// }
+
+// if(hasUser()) {
+//     header('location: /cadastro');
+// }
+
+// if (isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
+//     $nome = $_POST('nome');
+//     $email = $_POST('email');
+//     $senha = $_POST('senha');
+//     $db = connectDatabase();
+//     $db -> exec("INSERT INTO (user_name, user_email, user_senha) values('".$nome."', '".$email."', '".$senha."')");
+// }
+
+<?php
 
 require_once('../config/connect.php');
 require_once('../index.php');
 
-if(hasUser()) {
-    header('Location: /pages/dashbord.php');
-}
-
-if(hasUser()) {
-    header('location: /cadastro');
+if (hasUser()) {
+    header('Location: /pages/dashboard.php');
 }
 
 if (isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
-    $nome = $_POST('nome');
-    $email = $_POST('email');
-    $senha = $_POST('senha');
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
     $db = connectDatabase();
-    $db -> exec("INSERT INTO (user_name, user_email, user_senha) values('".$nome."', '".$email."', '".$senha."')");
+    $db->exec("INSERT INTO nome_da_sua_tabela (user_name, user_email, user_senha) VALUES ('" . $nome . "', '" . $email . "', '" . $senha . "')");
 }
+?>
